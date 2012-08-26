304527b1aac07363269f0c2caf407b1c3aa5345e
Sean Holden <Sean.Holden@o2.com>
2012-08-27 00:11:43 +0100

    initial commit with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfoundInstallations::Application.config.secret_token = 'ef60a2f4c01815b59ce273239374ff2011a8bb583c85bf770102dc99fa7d5b4f79b49096eecd03c3f2361129ec94a8ddea657715e2e1474627449bc419a3866e'
