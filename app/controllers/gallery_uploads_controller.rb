ed973d678cf4122eb989ee2db39d0c1b4474f2cd
SeanHolden <seanholden1@gmail.com>
2013-01-24 23:12:13 +0000

    added validations to ensure only images can be uploaded

--- app/controllers/gallery_uploads_controller.rb
+++ app/controllers/gallery_uploads_controller.rb
@@ -8,7 +8,8 @@ class GalleryUploadsController < ApplicationController
     album_title = params[:file_upload][:title]
     slug = album_title.downcase.strip.gsub(' ', '_').gsub(/[^\w-]/, '')
     album = Album.new(:title => album_title, :slug=>slug)
-    if album.save
+    if check_images_are_valid(images) == true
+      album.save
       gallery_id = album.id
       directory_location = "public/assets/workexamples/#{slug}"
       FileUtils.mkdir directory_location
@@ -22,9 +23,19 @@ class GalleryUploadsController < ApplicationController
   end
 
   private
+
+  def check_images_are_valid(images)
+    valid_extensions = ['jpg','jpeg','png','gif']
+    images.each do |image|
+      return false unless image.original_filename.include?('.')
+      extension = image.original_filename.split('.')[1]
+      extension = extension.downcase
+      return false unless valid_extensions.include?(extension)
+    end
+    return true
+  end
   
   def upload_images(images, directory_location, gallery_id)
-    images = params[:file_upload][:my_files]
     images.each do |image|
       tmp = image.tempfile
       file = File.join(directory_location, image.original_filename)
@@ -34,22 +45,3 @@ class GalleryUploadsController < ApplicationController
     end
   end
 end
-
-
-# TABLES:
-
-# Images
-
-# ID,   GALLERY_ID,   FILE_LOCATION
-# 1     1             '/workexamples/EPDM_Flat_roof1'
-# 2     1             '/workexamples/EPDM_Flat_roof2'
-# 3     1             '/workexamples/EPDM_Flat_roof3'
-# 4     2             '/workexamples/Wood_Grain_Back_Door1'
-# 5     2             '/workexamples/Wood_Grain_Back_Door2'
-
-
-# Album
-
-# ID     TITLE
-# 1      Flat roof
-# 2      Back door
