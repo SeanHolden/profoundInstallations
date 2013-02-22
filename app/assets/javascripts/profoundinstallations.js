c9878c7e9098a28a9a4e9d122d1ec392d91eb5f4
SeanHolden <sean.holden@o2.com>
2013-02-22 16:50:44 +0000

    Completed contact page along with javascript validations to only allow submit if all fields are filled in.

--- app/assets/javascripts/profoundinstallations.js
+++ app/assets/javascripts/profoundinstallations.js
@@ -11,10 +11,36 @@ window.onload = function () {
     }
   });
 
-  // 
+  // Scroll to fixed
   if ($(window).width() < 970){
    // Do nothing as this is mobile view
   }else{ // allow fixed position scrolling of side nav
     $('#services_nav').scrollToFixed({ marginTop: 20 });
   }
+
+  // Contact form
+  function submitDetails(){
+    if( allFilledIn() ){
+      return true;
+    } else {
+      alert('Please fill in ALL fields.');
+      return false;
+    }
+  };
+
+  function allFilledIn(){
+    var name    = $('#contact_name').val();
+    var email   = $('#contact_email').val();
+    var message = $('#contact_message').val();
+    if(name && email && message){
+      return true
+    }else{
+      return false
+    }
+  };
+
+  $("#contact_form").submit(function() {
+    return submitDetails();
+  });
+
 };
