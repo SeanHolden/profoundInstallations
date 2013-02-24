44916052241daa406599c627a8488691df9ca076
seanholden <seanholden1@gmail.com>
2013-02-24 20:29:02 +0000

    edited deploy files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,42 @@
-# require 'bundler/capistrano'
-# require "rvm/capistrano"
-
+require "bundler/capistrano"
+ 
+# General
 set :application, "profoundInstallations"
-set :repository,  "https://github.com/SeanHolden/profoundInstallations.git"
-
-set :scm, :git
-server "212.48.68.67", :web, :app, :db, :primary => true
-# ssh_options[:keys] = "~/.pem"
-
-set :deploy_to, "/home/sean/webapps/#{application}" 
-
-set :user, "sean" 
-# set :group, "sudo"
+set :domain, "198.211.124.44"
+# set :user, "deploy"
+# set :runner, "deploy"
 set :use_sudo, false
+set :deploy_to, "/var/www/#{application}"
+set :repository_cache, "#{application}_cache"
+set :environment, "production"
+ 
+# Roles
+role :web, domain
+role :app, domain
+role :db, domain, :primary => true
+ 
+# GIT
+set :repository, "git@github.com:SeanHolden/profoundInstallations.git"
+set :branch, "master"
+set :keep_releases, 3
+set :deploy_via, :remote_cache
+set :scm, :git
+ 
+# SSH
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ssh_options[:paranoid] = true # comment out if it gives you trouble. newest net/ssh needs this set.
+ 
+######## Callbacks - No More Config ########
+after 'deploy:symlink', 'deploy:cleanup' # makes sure there's only 3 deployments, deletes the extras
 
-# The below allows copy (no need for ssh keys, etc on the remote server)
-set :deploy_via, :copy
-set :copy_strategy, :export
-
-# assets precompile
-set :normalize_asset_timestamps, false
-
-
-# if you want to clean up old releases on each deploy do this:
-after "deploy:restart", "deploy:cleanup"
-
-# using Passenger mod_rails do this:
+# Custom Tasks
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  task(:start) {}
+  task(:stop) {}
+ 
+  desc "Restart Application"
+  task :restart, :roles => :web, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
